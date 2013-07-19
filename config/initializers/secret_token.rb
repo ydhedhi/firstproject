0389c47e46606fa9aa59f7f1ecaafd25ed126654
Yogesh Dhedhi <ydhedhi@entrib.com>
2013-07-19 15:59:17 +0530

    first cut

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Firstproject::Application.config.secret_key_base = 'c3fda6444982a22ac8d3a7ae8ae4d5174c1bbab5aee47c65b0a96af9901cb0d939903604ba48aa6bcb8905948edf9289cbc5cb3f74c33467cb8927cc60772b0b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Firstproject::Application.config.session_store :cookie_store, key: '_firstproject_session'
